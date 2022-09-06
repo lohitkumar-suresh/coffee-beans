8179fb9a9627eba30e5221e08e79a0b675ad25c5
Lohit Kumar <lohitkumar.suresh@gmail.com>
2022-09-05 23:47:36 -0400

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { BeanListComponent } from './bean/bean-list/bean-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'beans', pathMatch: 'full' },
+  { path: 'beans', component: BeanListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
