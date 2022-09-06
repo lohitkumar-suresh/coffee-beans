6c960969fe4ff4e6cd8f06c396c80fb5d8f8649f
Lohit Kumar <lohitkumar.suresh@gmail.com>
2022-09-06 18:46:12 -0400

    updating action

--- src/app/beans/state/beans.actions.ts
+++ src/app/beans/state/beans.actions.ts
@@ -1,8 +1,8 @@
 import { createAction, props } from "@ngrx/store";
 import { CoffeeBean } from "../model/bean";
 
-export const LOAD_BEANS = '[bean list page] load posts';
-export const LOAD_BEANS_SUCCESS = '[bean list page] load posts success';
+export const LOAD_BEANS = '[bean list page] load beans';
+export const LOAD_BEANS_SUCCESS = '[bean list page] load beans success';
 
 export const loadBeans = createAction(LOAD_BEANS);
 export const loadBeansSuccess = createAction(
